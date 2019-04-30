f19b1baba9393048abd5295c03bb60fe24022170
Fran Zekan <zekan.fran369@gmail.com>
2019-04-30 09:15:27 +0200

    Reorder button/text, fix styling

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,11 +2,17 @@ import React, { Component } from 'react'
 
 export default class TodoList extends Component {
   render() {
-    return this.props.todos.map((item, i) => (
+    const items = this.props.todos.map((item, i) => (
       <li key={i}>
-        {item}
         <button className="delete" onClick={() => this.props.delete(i)}>Delete</button>
+        {item}
       </li>
     ));
+
+    return (
+      <ul>
+        { items }
+      </ul>
+    );
   }
 }
